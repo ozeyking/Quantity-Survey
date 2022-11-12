8fbdf100a243850ea63ed0c392a4d0e86b45d594
6hislain <ghislainmike80@gmail.com>
2022-11-12 15:39:09 +0200

    home page

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from django.views.generic import RedirectView
 from . import views
 
-
-urlpatterns = []
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("signup", views.signup, name="signup"),
+    path("signin", views.signin, name="signin"),
+    path("logout", views.logout, name="logout"),
+]
