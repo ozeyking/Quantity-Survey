19700da8d60bce99c43401e8abc7d8b741680428
6hislain <ghislainmike80@gmail.com>
2022-12-22 11:58:28 +0200

    settings

--- main/urls.py
+++ main/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path("product/<int:id>/edit", views.product_edit, name="product.edit"),
     path("product/<int:id>/delete", views.product_delete, name="product.delete"),
     path("product/<int:id>", views.product_show, name="product.show"),
+    path("settings", views.settings, name="settings"),
+    path("profile/<int:id>", views.profile, name="profile"),
+    path("profile/<int:id>/update", views.profile_update, name="profile.update"),
 ]
--- main/views.py
+++ main/views.py
@@ -19,7 +19,55 @@ def home(request):
 @require_http_methods(["GET"])
 def profile(request, id):
     user = get_object_or_404(User, pk=id)
-    return JsonResponse(model_to_dict(user))
+
+    if not Profile.objects.filter(profile_user_id=id).exists():
+        profile = Profile.objects.create(user=user, profile_user_id=user.id)
+        profile.save()
+    else:
+        profile = Profile.objects.filter(profile_user_id=id).get()
+
+    return render(request, "profile.html", {"profile": profile, "user": user})
+
+
+@login_required(login_url="signin")
+@require_http_methods(["GET", "POST"])
+def settings(request):
+
+    if request.method == "GET":
+        profile = Profile.objects.filter(profile_user_id=request.user.id).first()
+
+        return render(
+            request,
+            "settings.html",
+            {"profile": profile},
+        )
+
+
+@require_http_methods(["POST"])
+@login_required(login_url="signin")
+def profile_update(request, id):
+    user = get_object_or_404(User, pk=request.user.id)
+
+    if not Profile.objects.filter(profile_user_id=id).exists():
+        profile = Profile.objects.create(user=user, profile_user_id=user.id)
+        profile.save()
+    else:
+        profile = Profile.objects.filter(profile_user_id=id).get()
+
+    user.last_name = request.POST["last_name"]
+    user.first_name = request.POST["first_name"]
+    user.username = request.POST["username"]
+    user.email = request.POST["email"]
+    profile.bio = request.POST["bio"]
+
+    if request.FILES.get("image") != None:
+        profile.image = request.FILES.get("image")
+
+    user.save()
+    profile.save()
+    messages.info(request, "Settings updated")
+
+    return redirect(request.META.get("HTTP_REFERER"))
 
 
 @require_http_methods(["GET"])
