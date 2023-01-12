2ffc8ebefd656a0f1f51237e3bd5be7dcd297f72
Mugemana Ghislain <ghislainmike80@gmail.com>
2023-01-12 11:09:03 +0200

    ...

--- main/views.py
+++ main/views.py
@@ -188,7 +188,7 @@ def product_create(request):
                 messages.info(request, "Product saved")
             except:
                 messages.info(
-                    request, "Some rows in the excel files contained invalid data"
+                    request, "Some rows in the excel sheet contain invalid data"
                 )
                 pass
         else:
