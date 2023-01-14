12319eb00feffea4f082ed805ec5751ca941e5ef
ozeyking <ozey250@gmail.com>
2023-01-14 13:06:52 +0200

    contact

--- main/views.py
+++ main/views.py
@@ -17,6 +17,8 @@ def home(request):
     return render(request, "home.html")
 def about(request):
     return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
 
 
 @require_http_methods(["GET"])
