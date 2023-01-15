aa2a48998bccdac9cf763f682b4a38084c0e364c
6hislain <ghislainmike80@gmail.com>
2023-01-15 16:04:36 +0200

    ..

---
+++ database/supervisor_site.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO `supervisor_site` (`id`, `name`, `address`, `description`, `supervisor_id`, `image`, `created_at`, `user_id`) VALUES
+(1, 'Kanombe', 'kigali', 'kanombe', 1, 'site/kanombe.jpg', '2023-01-15 10:53:56.493059', 3),
+(2, 'Nyagatare', '.', '.', 18, 'site/nyagatare.jpg', '2023-01-15 10:54:19.868550', 3),
+(3, 'Rwamagana', '.', '.', 19, 'site/rwamagana.jpg', '2023-01-15 10:55:00.547822', 3),
+(4, 'Huye', '.', '.', 20, 'site/huye.jpg', '2023-01-15 10:55:24.148156', 3),
+(5, 'Byumba', '.', '.', 21, 'site/byumba.jpg', '2023-01-15 10:55:53.240980', 3);
+
